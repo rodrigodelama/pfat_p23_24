f6fc5f8183af21e45ce3fc6095a75749f66ac851
Rodrigo De Lama <rodrigo.delama@icloud.com>
2024-03-13 14:09:52 +0100

    p1 DONE

--- p1/compile.sh
+++ p1/compile.sh
@@ -1,7 +1,7 @@
 # pfat_p23_24
 # From root directory of the project run the following commands:
 
-cd p1/java
+cd java
 javac -d ../class Errors/*.java
 javac -d ../class AST/*.java
 cd Parser
@@ -9,9 +9,10 @@ java -jar ../../class/java-cup-11b.jar parser
 mv *.java ../../class/
 cd ../../class/
 javac -cp ".;java-cup-11b-runtime.jar" -d . *.java
-rm parser.java
-rm sym.java
-cd ../java/Lexer
+rm *.java
+cd ../java
+javac -d ../class JLex/*.java
+cd Lexer
 java -cp ../../class JLex.Main Yylex
 mv Yylex.java ../../class/Yylex.java
 cd ../../class/
--- p1/java/Main.java
+++ p1/java/Main.java
@@ -24,8 +24,8 @@ public class Main
       try {
           in = new java.io.BufferedReader(new java.io.FileReader(args[0]));
           sc = new Yylex(in);
-          p = new parser(sc);
-          sroot = p.parse();
+          p = new parser(sc); 
+          sroot = p.parse(); //p.parser realiza el analisis sintactico
           System.out.println("Analisis lexico y sintactico correctos");
       } catch(IOException e) {
           System.out.println("Error abriendo fichero: " + args[0]);
